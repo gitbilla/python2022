6a3bff6b59f998f74088ef6c911858bc8a6f2486
gitbilla <augustine.abraham@outlook.com>
2017-12-26 07:16:46 +0530

    functions

---
+++ fun_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# Function definition is here
+def printinfo( name, age ):
+   "This prints a passed info into this function"
+   print "Name: ", name
+   print "Age ", age
+   return;
+
+# Now you can call printinfo function
+printinfo( age=50, name="miki" )
+
+def print_myinfo(name, age, school):
+	"This prints my information"
+	print "Name: ", name
+	print "Age: ", age
+	print "School: ", school
+print_myinfo (age=38, name="Augustine", school="St.Anns")
