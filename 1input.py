f90c352e39e8040361ede01cce4f2903232f5a5c
gitbilla <augustine.abraham@outlook.com>
2017-12-27 20:49:11 +0530

    1input.py,2list.py

---
+++ 1input.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+str = raw_input("Enter your Name : ");
+print "Hello  : ", str, "how are you ?";
+print "Iam Fine";
---
+++ 2list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+names = ['Augustine', 'Susan', 'Abigail', 'cyrus'];
+numbers = [1, 2, 3, 4, 5];
+print names[0], names[1];
+print numbers[1:6];
